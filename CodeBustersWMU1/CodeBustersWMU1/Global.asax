2e7fb2c0cd302840b6b2f206065b6b2aa9c6c12a
optimusprime <s143522@student.hb.se>
2017-01-23 17:55:44 +0100

    start

---
+++ CodeBustersWMU1/CodeBustersWMU1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodeBustersWMU1.MvcApplication" Language="C#" %>
---
+++ CodeBustersWMU1/CodeBustersWMU1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeBustersWMU1.Startup))]
+namespace CodeBustersWMU1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
