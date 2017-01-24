98795b8b16b7e9987ebaae53d1b23d58845d3044
optimusprime <s143522@student.hb.se>
2017-01-24 09:44:42 +0100

    test

--- CodeBustersWMU1/CodeBustersWMU1/Controllers/HomeController.cs
+++ CodeBustersWMU1/CodeBustersWMU1/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace CodeBustersWMU1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Om oss.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Kontakt.";
 
             return View();
         }
