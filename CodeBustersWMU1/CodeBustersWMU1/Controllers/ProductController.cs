7bf6dd52f61826234a88e74fa2a947cf7b4a5fae
optimusprime <s143522@student.hb.se>
2017-01-25 19:28:41 +0100

    senaste

--- CodeBustersWMU1/CodeBustersWMU1/Controllers/ProductController.cs
+++ CodeBustersWMU1/CodeBustersWMU1/Controllers/ProductController.cs
@@ -21,5 +21,12 @@ namespace CodeBustersWMU1.Controllers
             return View(db.Products.ToList());
         }
 
+        public ActionResult Description(int id)
+        {
+
+
+            return View(id);
+        }
+
     }
 }
