af26de7d7440ac8e268385c7c36d479c5e87e57a
optimusprime <s143522@student.hb.se>
2017-02-01 14:35:18 +0100

    dffdf

---
+++ CodeBustersWMU1/CodeBustersWMU1/Controllers/SearchController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeBustersWMU1.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult SearchDetails()
+        {
+            var product =
+from p in db.Products
+where p.ArticleId == id
+select p;
+            return View();
+        }
+    }
+}
---
+++ CodeBustersWMU1/CodeBustersWMU1/Models/Search.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeBustersWMU1.Models
+{
+    public class Search
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+
+        public void SearchOrder()
+        {
+            var product =
+    from p in db.Products
+    where p.ArticleId == id
+    select p;
+        } 
+
+    }
+}
